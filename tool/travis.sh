92246ebbc203f6ba8f7943ccdeb9864674f458a9
Michael Marner <michael@20papercups.net>
2018-09-26 08:20:09 +0930

    Only run tests once

--- tool/travis.sh
+++ tool/travis.sh
@@ -16,26 +16,26 @@ echo "Running tests..."
 pub run test --reporter expanded
 
 # Gather coverage and upload to Coveralls.
-  OBS_PORT=9292
-  echo "Collecting coverage on port $OBS_PORT..."
+OBS_PORT=9292
+echo "Collecting coverage on port $OBS_PORT..."
 
-  # Start tests in one VM.
-  dart \
-    --enable-vm-service=$OBS_PORT \
-    --pause-isolates-on-exit \
-    test/test_all.dart &
+# Start tests in one VM.
+dart \
+  --enable-vm-service=$OBS_PORT \
+  --pause-isolates-on-exit \
+  test/test_all.dart &
 
-  # Run the coverage collector to generate the JSON coverage report.
-  collect_coverage \
-    --port=$OBS_PORT \
-    --out=var/coverage.json \
-    --wait-paused \
-    --resume-isolates
+# Run the coverage collector to generate the JSON coverage report.
+collect_coverage \
+  --port=$OBS_PORT \
+  --out=var/coverage.json \
+  --wait-paused \
+  --resume-isolates
 
-  echo "Generating LCOV report..."
-  format_coverage \
-    --lcov \
-    --in=var/coverage.json \
-    --out=var/lcov.info \
-    --packages=.packages \
-    --report-on=lib
+echo "Generating LCOV report..."
+format_coverage \
+  --lcov \
+  --in=var/coverage.json \
+  --out=var/lcov.info \
+  --packages=.packages \
+  --report-on=lib
