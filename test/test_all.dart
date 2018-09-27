a5bfc3cc3c236f07d47d7d18bd788bdc3c7953d4
Michael Marner <michael@20papercups.net>
2018-09-27 19:51:34 +0930

    Add state encoder tests to ci test scritp

--- test/test_all.dart
+++ test/test_all.dart
@@ -1,9 +1,9 @@
-import 'package:test/test.dart';
-
 import 'action_encoder_test.dart' as actionEncoder;
+import 'state_encoder_test.dart' as stateEncoder;
 
 /// Script for running all tests on Travis CI
 /// Allows us to generate code coverage
 void main() {
   actionEncoder.main();
+  stateEncoder.main();
 }
