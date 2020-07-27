51b52678bcf3b337408a2a8dbaf8162a66546636
Michael Marner <michael@20papercups.net>
2020-07-27 15:38:24 +0930

    Put action encoder documentation back

--- lib/src/action_encoder.dart
+++ lib/src/action_encoder.dart
@@ -5,6 +5,19 @@ part of redux_remote_devtools;
 typedef ActionEncoder = String Function(dynamic action);
 
 /// An action encoder that converts an action to stringified JSON
+///
+/// Encodes an action as stringified JSON
+///
+/// Uses the form:
+///
+/// {
+///   "type": "TYPE"
+///   "payload": jsonEncode(action)
+/// }
+///
+/// Action type is set to be the class name for class based
+/// actions, or an enum value for enum actions
+///
 ActionEncoder JsonActionEncoder = (dynamic action) {
   /// Gets a type name for the action, based on the class name or value
   String getActionType(dynamic action) {
