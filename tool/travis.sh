51f3ed8e2f4837bf6edf4f0df4946105f22d3613
Michael Marner <michael@20papercups.net>
2021-03-22 22:07:39 +1030

    Fix travis

--- tool/travis.sh
+++ tool/travis.sh
@@ -8,29 +8,30 @@
 set -e
 
 # Run pub get to fetch packages.
-pub get
-pub global activate coverage
+dart pub get
+dart pub global activate coverage
 
 # Run the tests.
-echo "Running tests..."
-pub run test --reporter expanded
+# echo "Running tests..."
+# dart run test --reporter expanded
 
 # Gather coverage and upload to Coveralls.
 OBS_PORT=9292
 echo "Collecting coverage on port $OBS_PORT..."
 
-# Start tests in one VM.
-dart \
-  --enable-vm-service=$OBS_PORT \
-  --pause-isolates-on-exit \
-  test/test_all.dart &
-
 # Run the coverage collector to generate the JSON coverage report.
-collect_coverage \
+dart pub global run coverage:collect_coverage \
   --port=$OBS_PORT \
   --out=var/coverage.json \
   --wait-paused \
-  --resume-isolates
+  --resume-isolates &
+
+# Start tests in one VM.
+dart \
+  --disable-service-auth-codes \
+  --enable-vm-service=$OBS_PORT \
+  --pause-isolates-on-exit \
+  test/test_all.dart 
 
 echo "Generating LCOV report..."
 format_coverage \
